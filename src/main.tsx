b5d7b9be307e2686893c0aa21411089eaabbec3d
B1gDaddyKane <jenschrj97@gmail.com>
2023-01-16 21:06:12 +0100

    Added login page and changed title and icon in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
 ]);
 
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import SignupForm from '../components/forms/signupform';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default LoginPage;
